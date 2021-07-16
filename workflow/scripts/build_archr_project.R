1d9223f384e7f28198317d00a3e5e01786ccae37
Austin Wang <austin.wang1357@gmail.com>
2021-07-15 18:45:34 -0700

    fix R paths

--- workflow/scripts/build_archr_project.R
+++ workflow/scripts/build_archr_project.R
@@ -7,8 +7,9 @@ build_archr_project <- function(arrow_sample_names, input_paths, output_paths, t
     addArchRGenome("hg38")
 
     input_paths = unlist(input_paths)
+    # dir.create(output_paths[["arrows_temp_dir"]])
     arrow_output_names = paste0(output_paths[["arrows_temp_dir"]], arrow_sample_names)
-    print(arrow_output_names) ####
+    # print(arrow_output_names) ####
     arrows <- createArrowFiles(
         inputFiles = input_paths,
         sampleNames = arrow_sample_names,
