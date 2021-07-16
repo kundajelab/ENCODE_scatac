31c9df4e0024c37ebc0c58d7700793b8fa334773
Austin Wang <austin.wang1357@gmail.com>
2021-07-15 22:36:26 -0700

    fix R paths

--- workflow/scripts/build_archr_project.R
+++ workflow/scripts/build_archr_project.R
@@ -14,6 +14,8 @@ build_archr_project <- function(arrow_sample_names, input_paths, output_paths, t
 
     input_paths = unlist(input_paths)
     arrow_output_names = paste0(output_paths[["arrows_temp_dir"]], arrow_sample_names)
+    print(input_paths) ####
+    print(arrow_output_names) ####
     arrows <- createArrowFiles(
         inputFiles = input_paths,
         sampleNames = arrow_sample_names,
