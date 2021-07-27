8567f23dd72748c433aed78c948809b2f4b54bd0
Austin Wang <austin.wang1357@gmail.com>
2021-07-27 15:36:17 -0700

    align step

---
+++ workflow/scripts/align.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+FASTQ1_PATH = $1
+FASTQ2_PATH = $2
+BWT_IDX_PATH = $3
+
+MULTIMAPPING = $4
+
+BAM_RAW_PATH = $5
+BAM_NO_MITO_PATH = $6
+FLAGSTAT_QC_PATH = $7
+
+LOG_BWT_PATH = $8
+
+THREADS = $9
+
+if [ $MULTIMAPPING == 0 ]
+then
+    bowtie2 -X2000 --mm -x $BWT_IDX_PATH --threads $THREADS -1 $FASTQ1_PATH -2 $FASTQ2_PATH --sam-append-comment \ 
+    2> $LOG_BWT_PATH \
+    | samtools view -Su /dev/stdin \
+    | samtools sort - $BAM_RAW_PATH
+else
+    bowtie2 -k $((MULTIMAPPING+1)) -X2000 --mm -x $BWT_IDX_PATH --threads $THREADS -1 $FASTQ1_PATH -2 $FASTQ2_PATH --sam-append-comment \ 
+    2> $LOG_BWT_PATH \
+    | samtools view -Su /dev/stdin \
+    | samtools sort - $BAM_RAW_PATH
+fi
+
+samtools sort -n --@ $THREADS $BAM_RAW_PATH -O SAM | SAMstats --sorted_sam_file - --outf $FLAGSTAT_QC_PATH
+
+samtools idxstats $BAM_RAW_PATH | cut -f 1 | grep -v -P "^chrM$" | xargs samtools view $BAM_RAW_PATH -@ $THREADS -b> $BAM_NO_MITO_PATH
+
