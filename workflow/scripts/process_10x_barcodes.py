cd32a6534b9ad556b13bfe059259bd67e0842b5e
Surag Nair <10693069+suragnair@users.noreply.github.com>
2021-07-30 01:57:19 +0000

    Updated process 10x barcodes
    - Added max_dist as an input with default 2
    - Fixed small bug where maxdist was not propagated to gen_nearby_seqs
    - Now outputs a small qc tsv with fraction of corrected barcodes,
    fraction with 0,1,..errors etc.

--- workflow/scripts/process_10x_barcodes.py
+++ workflow/scripts/process_10x_barcodes.py
@@ -31,8 +31,9 @@ usage = "usage: %prog [options] [inputs] Software to process raw .fastq reads an
 opts.add_option("-a", "--fastq1", help="<Read1> Accepts fastq.gz")
 opts.add_option("-b", "--fastq2", help="<Read2> Accepts fastq.gz")
 opts.add_option("-c", "--fastq3", help="<Read3> Accepts fastq.gz")
-opts.add_option("-f", "--barcodesFile", help="<gzip of the universe of valid barcodes to check")
+opts.add_option("-f", "--barcodesFile", help="<gzip of the universe of valid barcodes (whitelist) to check")
 
+opts.add_option("-m", "--max-barcode-dist", default=2, type=int, help="Maximum edit distance allowed between a barcode and an entry in whitelist")
 opts.add_option("-n", "--nreads", default = 10000000, help="Number of reads to process in a given chunk")
 opts.add_option("-r", "--ncores", default = 8, help="Number of cores for parallel processing.")
 
@@ -40,21 +41,20 @@ opts.add_option("-o", "--output", help="Output sample convention")
 
 options, arguments = opts.parse_args()
 
-
 # return usage information if no argvs given
 if len(sys.argv)==1:
     os.system(sys.argv[0]+" --help")
     sys.exit()
-
 
 # Define barcodes
 barcodesfilepath = options.barcodesFile
 with gzip.open(barcodesfilepath, "rt") as my_file:
     barcodesR = my_file.readlines()
 barcodes = [barcode.rstrip() for barcode in barcodesR]
-print("Found and imported " + str(len(barcodes)) + " barcodes")    
-global barcodes_set 
+print("Found and imported " + str(len(barcodes)) + " barcodes")
+global barcodes_set
 barcodes_set = set(barcodes)
+
 
 def batch_iterator(iterator, batch_size):
     """
@@ -111,15 +111,14 @@ def formatRead(raw_barcode, corrected_barcode, title, sequence, quality):
 
 
 #-----
-# This code is modified from CellRanger-ATAC but I'm too lazy to factor in base qualities
+# This code is modified from CellRanger-ATAC, base qualities are not factored in however
 # https://github.com/10XGenomics/cellranger-atac/blob/main/mro/atac/stages/processing/attach_bcs/__init__.py
 #-----
 DNA_ALPHABET = 'AGCT'
 ALPHABET_MINUS = {char: {c for c in DNA_ALPHABET if c != char} for char in DNA_ALPHABET}
 ALPHABET_MINUS['N'] = set(DNA_ALPHABET)
-MAXDIST_CORRECT = 2
 
-def gen_nearby_seqs(seq,wl_idxs, maxdist=3):
+def gen_nearby_seqs(seq, maxdist=3):
     
     allowed_indices = [i for i in range(len(seq)) if seq[i] != 'N']
     required_indices = tuple([i for i in range(len(seq)) if seq[i] == 'N'])
@@ -140,17 +139,19 @@ def gen_nearby_seqs(seq,wl_idxs, maxdist=3):
 #------ 
 
 def correct_barcode(seq, maxdist=2):
-
+    # returns (corrected sequence, edit distance) if found
+    # else returns None
     if seq in barcodes_set:
-        return seq
+        return (seq, 0)
 
     for test_str in gen_nearby_seqs(seq, maxdist):
-        return(test_str)
+        edit_dist = len([i for i in range(len(test_str)) if test_str[i]!=seq[i]])
+        return(test_str, edit_dist)
 
-    return "N"*16
+    return (None, None)
 
     
-def debarcode_trio(trio):
+def debarcode_trio(trio, max_barcode_dist):
     """
     Function that is called in parallel
     """
@@ -166,21 +167,30 @@ def debarcode_trio(trio):
     title2 = listRead2[0]; sequence2 = listRead2[1]; quality2 = listRead2[2]
     title3 = listRead3[0]; sequence3 = listRead3[1]; quality3 = listRead3[2]
 
-    corrected_barcode = correct_barcode(sequence2)
+    corrected_barcode, edit_dist = correct_barcode(sequence2, maxdist=max_barcode_dist)
     #if(corrected_barcode != sequence2):
     #    print("was " + sequence2 + "   now: " + corrected_barcode)
     
     # Return the barcode with underscores + the biological sequence learned
-    ofq1 = formatRead(sequence2, corrected_barcode, title1, sequence1, quality1)
-    ofq2 = formatRead(sequence2, corrected_barcode, title3, sequence3, quality3)
-    return(ofq1, ofq2)
+    if corrected_barcode is not None:
+        ofq1 = formatRead(sequence2, corrected_barcode, title1, sequence1, quality1)
+        ofq2 = formatRead(sequence2, corrected_barcode, title3, sequence3, quality3)
+        return(ofq1, ofq2, edit_dist)
+    else:
+        return None
 
 
-if __name__ == "__main__":
+def write_qc(edit_dists, outfname):
+    with open(outfname, 'w') as f:
+        f.write("Total barcodes processed\t{}\n".format(len(edit_dists)))
+        f.write("Barcodes accepted\t{}\t{:.2f}\n".format(len(edit_dists)-edit_dists.count(-1), 100*(len(edit_dists)-edit_dists.count(-1))/len(edit_dists)))
+        for x in range(max(edit_dists)+1):
+            f.write("Barcodes with edit distance {}\t{}\t{:.2f}\n".format(x, edit_dists.count(x), 100*edit_dists.count(x)/len(edit_dists)))
 
-    
 
+if __name__ == "__main__": 
     ##### INPUTS #####
+    #import pdb;pdb.set_trace()
     af = options.fastq1
     bf = options.fastq2
     cf = options.fastq3
@@ -199,6 +209,8 @@ if __name__ == "__main__":
     else:
         sys.exit("ERROR! The input files (-a , -b, -c) a *.fastq.gz")
     print(options)
+    edit_dists = []
+
     with gzip.open(af, "rt") as f1:
         with gzip.open(bf, "rt") as f2:
                 with gzip.open(cf, "rt") as f3:
@@ -217,12 +229,16 @@ if __name__ == "__main__":
             
                         # parallel process the barcode processing and accounting of failures.
                         pool = Pool(processes=cpu)
-                        pm = pool.map(debarcode_trio, zip(batch1, batch2, batch3))
+
+                        pm = pool.starmap(debarcode_trio, zip(zip(batch1, batch2, batch3), [options.max_barcode_dist]*len(batch1)))
                         pool.close()
             
+                        # edit distances
+                        edit_dists += [item[2] if item is not None else -1 for item in pm]
+
                         # Aggregate output
-                        fastq1 = [item[0] for item in pm]
-                        fastq2 = [item[1] for item in pm]
+                        fastq1 = [item[0] for item in pm if item is not None]
+                        fastq2 = [item[1] for item in pm if item is not None]
 
                         # Export one chunk in parallel
                         filename1 = output +'_R1.fastq.gz'
@@ -231,5 +247,6 @@ if __name__ == "__main__":
                         pool = Pool(processes=2)
                         toke = pool.starmap(chunk_writer_gzip, [(filename1, fastq1), (filename2, fastq2)])
                         pool.close()
-            
+    
+    write_qc(edit_dists, output+"_barcode.qc.tsv")
     
