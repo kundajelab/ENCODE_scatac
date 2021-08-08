01b4fab4076a073cfeb6506ee3439910a52bfed8
Austin Wang <austin.wang1357@gmail.com>
2021-08-08 15:16:54 -0700

    full test

--- workflow/scripts/align.sh
+++ workflow/scripts/align.sh
@@ -18,25 +18,22 @@ THREADS=${10}
 mkdir -p $(dirname "$BAM_RAW_PATH")
 if [[ $MULTIMAPPING == 0 ]]
 then
-    bowtie2 -X2000 --mm -x $BWT_IDX_PATH --threads $THREADS -1 $FASTQ1_PATH -2 $FASTQ2_PATH --sam-append-comment \ 
-    2> $LOG_BWT_PATH \
-    | samtools view -u /dev/stdin \
-    | samtools sort -o $BAM_RAW_PATH - 
+    bowtie2 -X2000 --mm -x $BWT_IDX_PATH --threads $THREADS -1 $FASTQ1_PATH -2 $FASTQ2_PATH --sam-append-comment 2> $LOG_BWT_PATH | 
+    samtools view -u /dev/stdin | 
+    samtools sort -o $BAM_RAW_PATH -
 else
     bowtie2 -k $((MULTIMAPPING+1)) -X2000 --mm -x $BWT_IDX_PATH --threads $THREADS -1 $FASTQ1_PATH -2 $FASTQ2_PATH --sam-append-comment 2> $LOG_BWT_PATH | 
     samtools view -u /dev/stdin | 
     samtools sort -o $BAM_RAW_PATH -
 fi
 
-printf "asdfasdf\n"
+mkdir -p $(dirname "$FLAGSTAT_QC_PATH")
+samtools sort -n --@ $THREADS -O SAM $BAM_RAW_PATH | SAMstats --sorted_sam_file - --outf $FLAGSTAT_QC_PATH
 
-# mkdir -p $(dirname "$FLAGSTAT_QC_PATH")
-# samtools sort -n --@ $THREADS -O SAM $BAM_RAW_PATH | SAMstats --sorted_sam_file - --outf $FLAGSTAT_QC_PATH
+printf "ddddd\n"
 
-# printf "ddddd\n"
-
-# mkdir -p $(dirname "$BAM_NO_MITO_PATH")
-# samtools idxstats $BAM_RAW_PATH | cut -f 1 | grep -v -P "^chrM$" | xargs samtools view $BAM_RAW_PATH -@ $THREADS -b> $BAM_NO_MITO_PATH
-# mkdir -p $(dirname "$BAM_MITO_PATH")
-# samtools idxstats $BAM_RAW_PATH | cut -f 1 | grep -P "^chrM$" | xargs samtools view $BAM_RAW_PATH -@ $THREADS -b> $BAM_MITO_PATH
+mkdir -p $(dirname "$BAM_NO_MITO_PATH")
+samtools idxstats $BAM_RAW_PATH | cut -f 1 | grep -v -P "^chrM$" | xargs samtools view $BAM_RAW_PATH -@ $THREADS -b> $BAM_NO_MITO_PATH
+mkdir -p $(dirname "$BAM_MITO_PATH")
+samtools idxstats $BAM_RAW_PATH | cut -f 1 | grep -P "^chrM$" | xargs samtools view $BAM_RAW_PATH -@ $THREADS -b> $BAM_MITO_PATH
 
