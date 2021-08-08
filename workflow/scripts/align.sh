498a6706973c9f4f2106d55ed5647069ab41c217
Austin Wang <austin.wang1357@gmail.com>
2021-08-07 22:12:28 -0700

    full test

--- workflow/scripts/align.sh
+++ workflow/scripts/align.sh
@@ -20,12 +20,12 @@ then
     bowtie2 -X2000 --mm -x $BWT_IDX_PATH --threads $THREADS -1 $FASTQ1_PATH -2 $FASTQ2_PATH --sam-append-comment \ 
     2> $LOG_BWT_PATH \
     | samtools view -Su /dev/stdin \
-    | samtools sort - $BAM_RAW_PATH
+    | samtools sort -o $BAM_RAW_PATH - 
 else
     bowtie2 -k $((MULTIMAPPING+1)) -X2000 --mm -x $BWT_IDX_PATH --threads $THREADS -1 $FASTQ1_PATH -2 $FASTQ2_PATH --sam-append-comment \ 
     2> $LOG_BWT_PATH \
     | samtools view -Su /dev/stdin \
-    | samtools sort - $BAM_RAW_PATH
+    | samtools sort -o $BAM_RAW_PATH - 
 fi
 
 samtools sort -n --@ $THREADS $BAM_RAW_PATH -O SAM | SAMstats --sorted_sam_file - --outf $FLAGSTAT_QC_PATH
