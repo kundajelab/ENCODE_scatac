ae47b5a6079bd7f8dc499ee069a6f8194247df9c
Austin Wang <austin.wang1357@gmail.com>
2021-08-11 14:48:20 -0700

    matcha snakemake integration

--- workflow/scripts/process_10x_barcodes_matcha.py
+++ workflow/scripts/process_10x_barcodes_matcha.py
@@ -1,4 +1,3 @@
-import argparse
 import gzip
 
 import numpy as np
@@ -6,68 +5,69 @@ import tqdm
 
 import matcha
 
-# Processes 10x ATAC-seq fastqs
-# Inputs: 10x fastq files R1,R2,R3, and barcode whitelist
-# Outputs: 
-#   - R1.fastq.gz: Input R1 filtered for matching barcodes, with corrected barcode
-#     appended to the read name
-#   - R2.fastq.gz: Input R3 (NOT R2) filtered for matching barcodes, with corrected barcode
-#     appended to the read name
-#
-# Limitations:
-#  - Current QC output is just printing a few lines to stdout at the end of the run
-#  - Currently doesn't have arguments for the output filenames
-#  - Only outputs corrected barcode, skipping the uncorrected barcode.
+fastq1 = snakemake.input['fastq1']
+fastq2 = snakemake.input['fastq_bc']
+fastq3 = snakemake.input['fastq2']
+bc_whitelist = snakemake.input['bc_whitelist']
 
-def main():
-    parser = argparse.ArgumentParser(description='Process raw fastq reads to append cell barcodes')
-    parser.add_argument('fastq1', type=str, help='R1 fastq file')
-    parser.add_argument('fastq2', type=str, help='R2 fastq file')
-    parser.add_argument('fastq3', type=str, help='R3 fastq file')
-    parser.add_argument('barcodes', type=str, help='Gzip file with 10x barcode whitelist')
+max_barcode_dist = snakemake.params['max_barcode_dist']
 
-    parser.add_argument("--max-barcode-dist", type=int, default=2, help="Maximum edit distance allowed between a barcode and a whitelist entry")
-    parser.add_argument("--ncores", default=4, help="Number of cores for parallel processing")
-    
-    args = parser.parse_args()
-    f = matcha.FastqReader(threads = args.ncores)
-    f.add_sequence("R1", args.fastq1, output_path="R1.fastq.gz")
-    f.add_sequence("R2", args.fastq2)
-    f.add_sequence("R3", args.fastq3, output_path="R2.fastq.gz")
+fastq1_out_path = snakemake.output['fastq1_bc']
+fastq2_out_path = snakemake.output['fastq2_bc']
 
-    valid_10x_barcodes = [b.strip() for b in gzip.open(args.barcodes)]
+qc_path = snakemake.output['qc_barcode_corr']
+
+threads = snakemake.threads
+
+def get_open_fn(path):
+    with open(path, "rb") as f:
+        is_gzipped = (f.read(2) == b'\x1f\x8b')
+    return gzip.open if is_gzipped(path) else open
+
+def main(fastq1, fastq2, fastq3, bc_whitelist, max_barcode_dist, fastq1_out_path, fastq2_out_path, qc_path, threads):
+    f = matcha.FastqReader(threads = threads)
+    f.add_sequence("R1", fastq1, output_path=fastq1_out_path)
+    f.add_sequence("R2", fastq2)
+    f.add_sequence("R3", fastq3, output_path=fastq2_out_path)
+
+    open_fn = get_open_fn(bc_whitelist)
+    with open_fn(bc_whitelist) as f:
+        valid_10x_barcodes = [b.strip() for b in f]
 
     cell_barcode = matcha.HashMatcher(
         valid_10x_barcodes,
-        max_mismatches=args.max_barcode_dist,
+        max_mismatches=max_barcode_dist,
         subsequence_count=2
     )
 
     f.add_barcode("cell", cell_barcode, "R2")
 
-
     f.set_output_names("{read_name} CB:Z:{cell}")
 
-    barcode_counts = [0] * (args.max_barcode_dist + 1)
+    barcode_counts = [0] * (max_barcode_dist + 1)
     total_reads = 0
+    total_reads_in = 0
 
     chunk_size = 10000
     progress = tqdm.tqdm(disable=None, unit="reads")
-    while f.read_chunk(chunk_size):
-        pass_filter = (f.get_match_result("cell", "dist") <= args.max_barcode_dist) & \
+    while reads_in := f.read_chunk(chunk_size):
+        pass_filter = (f.get_match_result("cell", "dist") <= max_barcode_dist) & \
             (f.get_match_result("cell", "second_best_dist") > f.get_match_result("cell", "dist"))
 
         total_reads += len(pass_filter)
+        total_reads_in += reads_in
         values, counts = np.unique(f.get_match_result("cell", "dist"), return_counts=True)
         for v, c in zip(values, counts):
-            if v <= args.max_barcode_dist:
+            if v <= max_barcode_dist:
                 barcode_counts[v] += c
         
         f.write_chunk(pass_filter)
         progress.update(len(pass_filter))
 
-    for val, i in enumerate(barcode_counts):
-        print(val, i)
+    with open(qc_path, 'w') as f:
+        f.write(f"Total barcodes processed\t{total_reads_in}\n")
+        f.write(f"Barcodes accepted\t{total_reads}\t{total_reads/total_reads_in*100:.2f}\n")
+        for val, i in enumerate(barcode_counts):
+            f.write(f"Barcodes with edit distance {i}\t{val}\t{val/total_reads_in*100:.2f}\n")
 
-if __name__ == "__main__":
-    main()
+main(fastq1, fastq2, fastq3, bc_whitelist, max_barcode_dist, fastq1_out_path, fastq2_out_path, qc_path, threads)
