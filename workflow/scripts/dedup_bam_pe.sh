3ce8dfbfa28908e0d50250068cdb7d9df681af64
Austin Wang <austin.wang1357@gmail.com>
2021-08-11 21:59:17 -0700

    clean up shell scripts

--- workflow/scripts/dedup_bam_pe.sh
+++ workflow/scripts/dedup_bam_pe.sh
@@ -39,28 +39,17 @@ else
     samtools view -h ${TMP_FILT_BAM_FILE} | python ${MMP_PATH} -k $MULTIMAPPING --paired-end | samtools fixmate -r /dev/stdin ${TMP_FILT_FIXMATE_BAM_FILE}
 fi
 
-# Remove orphan reads (pair was removed)
-# and read pairs mapping to different chromosomes
-# Obtain position sorted BAM
-
-
 samtools view -F 1804 -f 2 -u ${TMP_FILT_FIXMATE_BAM_FILE} | samtools sort -o ${FILT_BAM_FILE} -
 
 rm ${TMP_FILT_FIXMATE_BAM_FILE}
-
 rm ${TMP_FILT_BAM_FILE}
 
 # =============
 # Mark duplicates
 # =============
-# module add picard-tools/1.126
 
 TMP_FILT_BAM_FILE="${FILT_BAM_PREFIX}.dupmark.bam"
-# MARKDUP="/srv/gs1/software/picard-tools/1.126/MarkDuplicates.jar"
-# DUP_FILE_QC="${FILT_BAM_PREFIX}.dup.qc"
-
 picard MarkDuplicates INPUT=${FILT_BAM_FILE} OUTPUT=${TMP_FILT_BAM_FILE} METRICS_FILE=${DUP_FILE_QC} VALIDATION_STRINGENCY=LENIENT ASSUME_SORTED=true REMOVE_DUPLICATES=false BARCODE_TAG=CB
-
 mv ${TMP_FILT_BAM_FILE} ${FILT_BAM_FILE}
 
 # ============================
@@ -68,17 +57,9 @@ mv ${TMP_FILT_BAM_FILE} ${FILT_BAM_FILE}
 # Index final position sorted BAM
 # Create final name sorted BAM
 # ============================
-# FINAL_BAM_PREFIX="${OFPREFIX}.nodup"
-# FINAL_BAM_FILE="${FINAL_BAM_PREFIX}.bam" # To be stored
-# FINAL_BAM_INDEX_FILE="${FINAL_BAM_FILE}.bai"
-# FINAL_BAM_FILE_MAPSTATS="${FINAL_BAM_PREFIX}.flagstat.qc" # QC file
 
 samtools view -F 1804 -f 2 -b ${FILT_BAM_FILE} > ${FINAL_BAM_FILE}
-
-
-# Index Final BAM file
 samtools index ${FINAL_BAM_FILE}
-
 samtools sort -n -@ ${THREADS} ${FINAL_BAM_FILE} -O SAM  | SAMstats --sorted_sam_file -  --outf ${FINAL_BAM_FILE_MAPSTATS}
 
 # =============================
@@ -89,15 +70,11 @@ samtools sort -n -@ ${THREADS} ${FINAL_BAM_FILE} -O SAM  | SAMstats --sorted_sam
 # sort by position and strand
 # Obtain unique count statistics
 
-# module add bedtools/2.26
-
-# PBC_FILE_QC="${FINAL_BAM_PREFIX}.pbc.qc"
-
-# TotalReadPairs [tab] DistinctReadPairs [tab] OneReadPair [tab] TwoReadPairs [tab] NRF=Distinct/Total [tab] PBC1=OnePair/Distinct [tab] PBC2=OnePair/TwoPair
-
-
 samtools sort -n ${FILT_BAM_FILE} -o ${TEMP_PREFIX}/srt.tmp.bam
-bedtools bamtobed -bedpe -i ${TEMP_PREFIX}/srt.tmp.bam | awk 'BEGIN{OFS="\t"}{print $1,$2,$4,$6,$9,$10}' | grep -v 'chrM' | sort | uniq -c | awk 'BEGIN{mt=0;m0=0;m1=0;m2=0} ($1==1){m1=m1+1} ($1==2){m2=m2+1} {m0=m0+1} {mt=mt+$1} END{printf "%d\t%d\t%d\t%d\t%f\t%f\t%f\n",mt,m0,m1,m2,m0/mt,m1/m0,m1/m2}' > ${PBC_FILE_QC}
-# rm ${OFPREFIX}.srt.tmp.bam
+
+bedtools bamtobed -bedpe -i ${TEMP_PREFIX}/srt.tmp.bam | 
+awk 'BEGIN{OFS="\t"}{print $1,$2,$4,$6,$9,$10}' | 
+grep -v 'chrM' | sort | uniq -c | 
+awk 'BEGIN{mt=0;m0=0;m1=0;m2=0} ($1==1){m1=m1+1} ($1==2){m2=m2+1} {m0=m0+1} {mt=mt+$1} END{printf "%d\t%d\t%d\t%d\t%f\t%f\t%f\n",mt,m0,m1,m2,m0/mt,m1/m0,m1/m2}' > ${PBC_FILE_QC}
 
 rm ${FILT_BAM_FILE}
