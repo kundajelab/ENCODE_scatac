e64038b2b435eb69da8811fdc78eba2e31dbd865
Austin Wang <austin.wang1357@gmail.com>
2021-09-05 14:40:22 -0700

    parse archr qc

--- workflow/scripts/parse_archr_qc.R
+++ workflow/scripts/parse_archr_qc.R
@@ -7,6 +7,7 @@ r <- readRDS(in_path_meta)
 write.table(r, out_path_meta, sep = '\t', row.names = FALSE, quote = FALSE)
 
 r <- readRDS(in_path_doublets)
+print(names(r)) ####
 res <- r[['doubletResults']]
 print(names(res)) ####
 print(names(res[['doubletEnrichLSI']])) ####
