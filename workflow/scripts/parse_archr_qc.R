369874e7e2967be4421781b4985caf910e4eea6c
Austin Wang <austin.wang1357@gmail.com>
2021-09-07 14:34:05 -0700

    parse archr qc

--- workflow/scripts/parse_archr_qc.R
+++ workflow/scripts/parse_archr_qc.R
@@ -18,7 +18,7 @@ r <- readRDS(in_path_doublets)
 # print(length(r$doubletResults$doubletEnrichUMAP)) ####
 # print(length(r$doubletResults$doubletEnrichLSI)) ####
 # print(length(r$doubletResults$doubletScoreLSI)) ####
-print(names(r$originalDataUMAP)) ####
+print(r$originalDataUMAP$X1) ####
 
 res <- r[['doubletResults']]
 d <- data.frame(
