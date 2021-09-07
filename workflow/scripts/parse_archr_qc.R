d17d5f2ecf72480b2c520e162db3f3fc859ab2a9
Austin Wang <austin.wang1357@gmail.com>
2021-09-07 14:44:39 -0700

    parse archr qc

--- workflow/scripts/parse_archr_qc.R
+++ workflow/scripts/parse_archr_qc.R
@@ -18,19 +18,19 @@ r <- readRDS(in_path_doublets)
 # print(length(r$doubletResults$doubletEnrichUMAP)) ####
 # print(length(r$doubletResults$doubletEnrichLSI)) ####
 # print(length(r$doubletResults$doubletScoreLSI)) ####
-print(names(r$originalDataUMAP)) ####
-print(head(r$originalDataUMAP)) ####
-print(row.names(r$originalDataUMAP)) ####
+# print(names(r$originalDataUMAP)) ####
+# print(head(r$originalDataUMAP)) ####
+# print(row.names(r$originalDataUMAP)) ####
 
 res <- r[['doubletResults']]
 d <- data.frame(
-    # barcode = as.vector(names(r$originalDataUMAP)), 
+    barcode = row.names(r$originalDataUMAP), 
     doubletScoreUMAP = res$doubletScoreUMAP,
     doubletEnrichUMAP = res$doubletEnrichUMAP,
     doubletEnrichLSI = res$doubletEnrichLSI, 
     doubletScoreLSI = res$doubletScoreLSI
 )
-# print(d) ####
+print(head(d)) ####
 # write.table(d, out_path_doublets, sep = '\t', row.names = FALSE, quote = FALSE)
 
 # sink(type = "message")
