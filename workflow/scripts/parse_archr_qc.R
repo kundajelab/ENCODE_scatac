3dd699108ec5f3fab3003d022b8af5b4d96e0286
Austin Wang <austin.wang1357@gmail.com>
2021-09-07 14:19:18 -0700

    parse archr qc

--- workflow/scripts/parse_archr_qc.R
+++ workflow/scripts/parse_archr_qc.R
@@ -12,21 +12,22 @@ write.table(r, out_path_meta, sep = '\t', row.names = FALSE, quote = FALSE)
 
 r <- readRDS(in_path_doublets)
 # print(r$simulatedDoubletUMAP) ####
-print(length(r$originalDataUMAP$score)) ####
-print(length(r$originalDataUMAP$enrichment)) ####
-print(length(r$doubletResults$doubletScoreUMAP)) ####
-print(length(r$doubletResults$doubletEnrichUMAP)) ####
-print(length(r$doubletResults$doubletEnrichLSI)) ####
-print(length(r$doubletResults$doubletScoreLSI)) ####
-
-
+# print(length(r$originalDataUMAP$score)) ####
+# print(length(r$originalDataUMAP$enrichment)) ####
+# print(length(r$doubletResults$doubletScoreUMAP)) ####
+# print(length(r$doubletResults$doubletEnrichUMAP)) ####
+# print(length(r$doubletResults$doubletEnrichLSI)) ####
+# print(length(r$doubletResults$doubletScoreLSI)) ####
 
 res <- r[['doubletResults']]
 d <- data.frame(
-    # barcode = as.vector(names(res[['doubletEnrichLSI']])), 
-    doubletEnrichLSI = res[['doubletEnrichLSI']], 
-    doubletScoreLSI = res[['doubletScoreLSI']]
+    barcode = as.vector(names(r$originalDataUMAP)), 
+    doubletScoreUMAP = res$doubletScoreUMAP,
+    doubletEnrichUMAP = res$doubletEnrichUMAP,
+    doubletEnrichLSI = res$doubletEnrichLSI, 
+    doubletScoreLSI = res$doubletScoreLSI
 )
+print(d) ####
 # write.table(d, out_path_doublets, sep = '\t', row.names = FALSE, quote = FALSE)
 
 # sink(type = "message")
