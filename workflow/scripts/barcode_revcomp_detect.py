b65ccff5be3c03a42dd64659049e8c0d15f7177f
Austin Wang <austin.wang1357@gmail.com>
2021-09-21 09:02:16 -0700

    Change download method to curl

--- workflow/scripts/barcode_revcomp_detect.py
+++ workflow/scripts/barcode_revcomp_detect.py
@@ -1,5 +1,3 @@
-# import gzip ####
-
 REV_COMP = str.maketrans("ATGC", "TACG")
 def reverse_complement(seq):
     return str.translate(seq, REV_COMP)[::-1]
@@ -22,33 +20,22 @@ def bc_detect(fastq, whitelist, out, qc, offset, num_reads=1000, thresh=0.8):
     bc_match = 0
     bcrc_match = 0
     num_lines = num_reads * 4
-    # with gzip.open(fastq, 'rt') as f: ####
-    print("iwheogihwoeghiwe") ####
-    with open(fastq, 'rt') as f: ####
+    with open(fastq, 'rt') as f:
         for lnum, line in enumerate(f):
-            print("wogy9384yt93") ####
-            print(lnum, line) ####
             if lnum >= num_lines:
                 break
             if lnum % 4 != 1:
                 continue
             seq = line.strip()[offset:]
-            print(seq) ####
             if seq in bc:
                 bc_match += 1
             if seq in bcrc:
                 bcrc_match += 1
 
-    # with open(fastq, 'rt') as f: ####
-    #     for lnum, line in enumerate(f):
-    #         print("eiwoe") ####
-    #         print(lnum, line) ####
-
     bc_match_prop = bc_match / num_reads
     bcrc_match_prop = bcrc_match / num_reads
-    valid = (bc_match_prop >= thresh) and (bcrc_match_prop >= thresh)
+    valid = (bc_match_prop >= thresh) or (bcrc_match_prop >= thresh)
     rc_chosen = (bc_match_prop >= bcrc_match_prop)
-    print(bc_match, bcrc_match) ####
 
     with open(qc, 'w') as f:
         f.write(f"Direct match proportion: {bc_match_prop}\n")
@@ -71,7 +58,6 @@ try:
 
     fastq = snakemake.input["fastq"]
     whitelist = snakemake.input["whitelist"]
-    # print("test") ####
 
     if modality == "10x":
         offset = 0
