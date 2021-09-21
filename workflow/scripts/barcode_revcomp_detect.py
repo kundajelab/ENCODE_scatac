ef6ec6507cb2a22ea2f22d382d4f4d6a7d474393
Austin Wang <austin.wang1357@gmail.com>
2021-09-20 21:53:07 -0700

    Change download method to curl

--- workflow/scripts/barcode_revcomp_detect.py
+++ workflow/scripts/barcode_revcomp_detect.py
@@ -1,5 +1,3 @@
-import gzip
-
 import numpy as np
 
 REV_COMP = str.maketrans("ATGC", "TACG")
@@ -24,7 +22,7 @@ def bc_detect(fastq, whitelist, out, qc, offset, num_reads=1000, thresh=0.8):
     bc_match = 0
     bcrc_match = 0
     num_lines = num_reads * 4
-    with gzip.open(fastq, 'rt') as f:
+    with open(fastq, 'rt') as f:
         for lnum, line in enumerate(f):
             if lnum >= num_lines:
                 break
