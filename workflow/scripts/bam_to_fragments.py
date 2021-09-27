e16bd13d0c4b5af8f86a915ae223908467cdcb01
Austin Wang <austin.wang1357@gmail.com>
2021-09-27 13:44:44 -0700

    Autodetect cutoffs for multiplet filtering

--- workflow/scripts/bam_to_fragments.py
+++ workflow/scripts/bam_to_fragments.py
@@ -26,7 +26,7 @@ def bam_to_frag(in_path, out_path, shift_plus=4, shift_minus=-4):
             if pos == curr_pos:
                 buf.append(data)
             else:
-                buf.sort()
+                # buf.sort()
                 for i in buf:
                     print(*i, sep="\t", file=out_file)
                 buf.clear()
