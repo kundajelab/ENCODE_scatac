537d9720bf3bfc6d293468438a9b4ac3e0723e37
Austin Wang <austin.wang1357@gmail.com>
2021-10-27 12:30:51 -0700

    Validate config

---
+++ utils/check_config.py
@@ -0,0 +1,54 @@
+import sys
+from encode_utils.connection import Connection
+
+def fetch_info(experiment, dcc_mode):
+
+    conn = Connection(dcc_mode)
+    data = conn.get(experiment)
+
+    lab = data["lab"]["name"]
+    multiome = data["related_series"].get("accession")
+    
+    return lab, multiome
+
+def load_samples(sample_file):
+    with open("config/samples.tsv") as sample_file:
+        h = sample_file.readline().rstrip('\n').split('\t')
+        exp_ind = h.index("Experiment")
+        rep_ind = h.index("Replicate")
+        mod_ind = h.index("Modality")
+        gen_ind = h.index("Genome")
+        samples = []
+        for line in sample_file:
+            if line.startswith("#"):
+                continue
+            if line.startswith("@"):
+                continue
+            if line.startswith("$"):
+                continue
+            entries = line.rstrip('\n').split('\t')
+            exp = entries[exp_ind]
+            rep = int(entries[rep_ind])
+            mod = entries[mod_ind]
+            gen = entries[gen_ind]
+            data = [
+                exp,
+                rep,
+                mod,
+                gen
+            ]
+            samples.append(data)
+    return samples
+
+def check_config(sample_file, out_path, dcc_mode):
+    samples = load_samples(sample_file)
+    with open(out_path, "w") as out_file:
+        for s in samples:
+            lab, mult = fetch_info(s[0], dcc_mode)
+            print(*(s + [lab, mult]), sep="\t", file=out_file)
+
+if __name__ == 'main':
+    sample_file = sys.argv[1]
+    out_path = sys.argv[2]
+    dcc_mode = "prod"
+    check_config(sample_file, out_path, dcc_mode)
